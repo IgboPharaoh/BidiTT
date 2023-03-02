279a4577fc60b31541dc2a93b28d8636a0dab9c2
IgboPharaoh <solomoneze62@gmail.com>
2023-03-02 20:48:32 +0100

    created reusable input component

---
+++ client/src/components/CustomInput.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { InputGroup, Text, Input } from '@chakra-ui/react';
+import { InputProps } from '../interfaces';
+
+const CustomInput = ({ labelName, ...props }: InputProps) => {
+    return (
+        <InputGroup flexDir='column'>
+            <Text fontSize='14px' color='#2d2d2d' opacity={'85%'} fontWeight='500' mb='4px'>
+                {labelName}
+            </Text>
+            <Input height='42px' _placeholder={{ color: '#2d2d2d', opacity: '50%' }} {...props} />
+        </InputGroup>
+    );
+};
+
+export default CustomInput;
